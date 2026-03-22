90aebbc20055df90fe8fb664ecef5df87e724139
betongame-web <asimhalder612@gmail.com>
2026-03-22 23:35:49 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import SelectRole from "./pages/SelectRole";
 import Profile from "./pages/Profile";
 import Deposit from "./pages/Deposit";
 
@@ -8,6 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/select-role" element={<SelectRole />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/deposit" element={<Deposit />} />
       </Routes>
